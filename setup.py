7f89f3849499e74c961a28b67dc6802744dd38f2
Bhanu Prakash <bhanuprakashkalla.1995@gmail.com@example.com>
2025-07-05 02:54:31 +0530

    all files updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="bhanu",
+    author_email="bhanuprakashkalla.1995@gmail.com",
+    packages=find_packages()
+)
